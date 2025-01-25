6cc2a0f03ea8a7d9553a8004615c0d71cdd07aef
avlnkov <alex.lenkov@mail.ru>
2025-01-25 21:52:09 +0300

    l3: реализовал уведомления для API < 26. На таких устройствах оно будет работать

--- app/src/main/java/com/example/servicesapp/MainActivity.kt
+++ app/src/main/java/com/example/servicesapp/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.servicesapp
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.servicesapp.databinding.ActivityMainBinding
@@ -17,5 +20,22 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
+
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notification = Notification.Builder(this) // Создаём уведомление
+            .setContentTitle("Title") // Заголовок
+            .setContentText("Text") // Текст
+            .setSmallIcon(R.drawable.ic_launcher_background) // Передаем иконку
+            .build() // Строим
+
+        // Отображение уведомления
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(1, notification)
     }
 }
