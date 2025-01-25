4b1f9fe2b1d8507b5da7aa03dc6296004b5bfb7a
avlnkov <alex.lenkov@mail.ru>
2025-01-25 22:09:10 +0300

    l3: реализовал уведомления для API > 26. На таких устройствах оно будет работать

--- app/src/main/java/com/example/servicesapp/MainActivity.kt
+++ app/src/main/java/com/example/servicesapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.servicesapp
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.example.servicesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -27,15 +29,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showNotification() {
-        val notification = Notification.Builder(this) // Создаём уведомление
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        // Реализовывать можно начиная с API > 26
+        // Добавляем проверку
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        // Вызываем NotificationCompat, чтобы работать от API > 26
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID) // Создаём уведомление
             .setContentTitle("Title") // Заголовок
             .setContentText("Text") // Текст
             .setSmallIcon(R.drawable.ic_launcher_background) // Передаем иконку
             .build() // Строим
 
         // Отображение уведомления
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
         notificationManager.notify(1, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
